dcda91a7fe41c6d7972065f73ee2d76144de953d
Oenz <74658821+Oenz@users.noreply.github.com>
2021-04-30 16:00:11 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -18,13 +18,16 @@ SlotRange = 10;
 @bot.command()
 async def slot(ctx):
     SlotList = list()
-    
+    #random.randrange(SlotRange)
     for i in range(3):
-        SlotStr = ":emoji_" + str(random.randrange(SlotRange)) + ':'
+        SlotStr = ":emoji_" + str(1) + ':'
         SlotList.append(SlotStr)
     
     SlotResult = SlotList[0] + ' ' + SlotList[1] + ' ' + SlotList[2]
     
     await ctx.send(SlotResult)
+    
+    if SlotList[0] == SlotList[1] == SlotList[2]:
+        await ctx.send('https://tenor.com/view/%E3%81%8A%E3%82%81%E3%81%A7%E3%81%A8%E3%81%86-%E5%AC%89%E3%81%97%E3%81%84-%E3%82%AF%E3%83%A9%E3%83%83%E3%82%AB%E3%83%BC-%E3%81%8A%E7%A5%9D%E3%81%84-%E5%8F%AF%E6%84%9B%E3%81%84-gif-15780951')
 
 bot.run(token)
