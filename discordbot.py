ad7dc315e84ed4e6d82edad4fc28db9df4eec57a
Oenz <74658821+Oenz@users.noreply.github.com>
2021-04-30 16:17:55 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -13,11 +13,11 @@ async def on_command_error(ctx, error):
     await ctx.send(error_msg)
 
 
-SlotRange = 10;
+SlotRange = [ 1, 2, 3, 4, 5, 6, 12, 13, 14 ];
 SlotWin = [
     "https://tenor.com/view/%E3%81%8A%E3%82%81%E3%81%A7%E3%81%A8%E3%81%86-%E5%AC%89%E3%81%97%E3%81%84-%E3%82%AF%E3%83%A9%E3%83%83%E3%82%AB%E3%83%BC-%E3%81%8A%E7%A5%9D%E3%81%84-%E5%8F%AF%E6%84%9B%E3%81%84-gif-15780951"
     ,"https://tenor.com/view/%E3%81%8A%E3%82%81%E3%81%A7%E3%81%A8%E3%81%86-%E5%AC%89%E3%81%97%E3%81%84-%E7%B4%99%E5%90%B9%E9%9B%AA-%E3%81%8A%E7%A5%9D%E3%81%84-%E5%8F%AF%E6%84%9B%E3%81%84-gif-15876321"
-    ,"https://discordapp.com/channels/435268850561318942/435268850561318944/837586554662289429"
+    ,"https://tenor.com/view/krol-gif-19996608"
     ]
 
 @bot.command()
@@ -25,9 +25,9 @@ async def slot(ctx):
     SlotList = list()
     #random.randrange(SlotRange)
     for i in range(3):
-        SlotStr = ":emoji_" + str(1) + ':'
+        SlotStr = ":emoji_" + str(random.choice(SlotRange)) + ':'
         SlotList.append(SlotStr)
-        SlotResult = ' ' + SlotList[i]
+        SlotResult += ' ' + SlotList[i]
     
     await ctx.send(SlotResult)
     
