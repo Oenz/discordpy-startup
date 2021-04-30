18dbd820731f328d1166d3f7e3a9e872200598f8
Oenz <74658821+Oenz@users.noreply.github.com>
2021-04-30 16:11:37 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,7 +14,12 @@ async def on_command_error(ctx, error):
 
 
 SlotRange = 10;
-    
+SlotWin = [
+    "https://tenor.com/view/%E3%81%8A%E3%82%81%E3%81%A7%E3%81%A8%E3%81%86-%E5%AC%89%E3%81%97%E3%81%84-%E3%82%AF%E3%83%A9%E3%83%83%E3%82%AB%E3%83%BC-%E3%81%8A%E7%A5%9D%E3%81%84-%E5%8F%AF%E6%84%9B%E3%81%84-gif-15780951"
+    ,"https://tenor.com/view/%E3%81%8A%E3%82%81%E3%81%A7%E3%81%A8%E3%81%86-%E5%AC%89%E3%81%97%E3%81%84-%E7%B4%99%E5%90%B9%E9%9B%AA-%E3%81%8A%E7%A5%9D%E3%81%84-%E5%8F%AF%E6%84%9B%E3%81%84-gif-15876321"
+    ,"https://discordapp.com/channels/435268850561318942/435268850561318944/837586554662289429"
+    ]
+
 @bot.command()
 async def slot(ctx):
     SlotList = list()
@@ -22,12 +27,11 @@ async def slot(ctx):
     for i in range(3):
         SlotStr = ":emoji_" + str(1) + ':'
         SlotList.append(SlotStr)
-    
-    SlotResult = SlotList[0] + ' ' + SlotList[1] + ' ' + SlotList[2]
+        SlotResult = ' ' + SlotList[i]
     
     await ctx.send(SlotResult)
     
     if SlotList[0] == SlotList[1] == SlotList[2]:
-        await ctx.send('https://tenor.com/view/%E3%81%8A%E3%82%81%E3%81%A7%E3%81%A8%E3%81%86-%E5%AC%89%E3%81%97%E3%81%84-%E3%82%AF%E3%83%A9%E3%83%83%E3%82%AB%E3%83%BC-%E3%81%8A%E7%A5%9D%E3%81%84-%E5%8F%AF%E6%84%9B%E3%81%84-gif-15780951')
+        await ctx.send(random.choice(SlotWin))
 
 bot.run(token)
