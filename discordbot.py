39327945d3b465458b50c10f511ac996dd4a6726
Oenz <74658821+Oenz@users.noreply.github.com>
2021-11-21 00:04:06 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -76,7 +76,17 @@ async def branchserver(ctx):
     guild = bot.get_guild(ctx.guild.id)
     await guild.delete()
     
-    
+@bot.command(name='delete-channel', help='delete a channel with the specified name')
+async def delete_channel(ctx, channel_name):
+   # check if the channel exists
+   existing_channel = discord.utils.get(guild.channels, name=channel_name)
+   
+   # if the channel exists
+   if existing_channel is not None:
+      await existing_channel.delete()
+   # if the channel does not exist, inform the user
+   else:
+      await ctx.send(f'No channel named, "{channel_name}", was found')    
     
     
     
