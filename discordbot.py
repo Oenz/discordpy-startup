d75735ec2c510b4c5482daeaf0c47c315db9a63b
Oenz <74658821+Oenz@users.noreply.github.com>
2021-11-21 01:17:39 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -78,7 +78,7 @@ async def branchserver(ctx):
     
 @bot.command()
 async def dlp(ctx):
-    channel = bot.get_channel(911651040317812796)
+    channel = bot.get_channel(673867257893486592)
     await channel.delete()
     
 @bot.command(name='delete-channel', help='delete a channel with the specified name')
